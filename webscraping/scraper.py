81d8680c0e924147de4d1901c98623221d7a7e57
Ta7ar <taterazay99@gmail.com>
2022-01-07 23:43:31 -0500

    Changed list append to extend

--- webscraping/scraper.py
+++ webscraping/scraper.py
@@ -48,44 +48,14 @@ class CNBC(Scraper):
         article_text = ''.join([tag.get_text() for tag in article_p_tags])
 
         return Article(article_title,article_text,"CNBC",article_publish_date,link)
-
-# def _cnbc():
-#     source = request.urlopen("https://www.cnbc.com/").read()
-#     soup = BeautifulSoup(source,'lxml')
-
-#     # Get list of article links from front page
-#     thumbnail_tags = soup.find('div',attrs={'id':'homepage-riverPlus'}).find_all('div',attrs={'class':'RiverHeadline-headline RiverHeadline-hasThumbnail'})
-#     links = [thumbnail.find('a').attrs['href'] for thumbnail in thumbnail_tags]
-#     links = [link for link in links if link != '/pro/']
-
-#     # Parse the article from each link
-#     articles = []
-#     for link in links:
-#         try:
-#             source = request.urlopen(link).read()
-#             soup = BeautifulSoup(source,'lxml')
-
-#             article_title = soup.title.get_text()
-
-#             article_publish_date = soup.find('time',attrs={'itemprop':'datePublished'}).attrs['datetime']
-
-#             article_p_tags = soup.find('div',attrs={'class':'ArticleBody-articleBody'}).find_all('p')
-#             article_text = ''.join([tag.get_text() for tag in article_p_tags])
-
-#             article = Article(article_title,article_text,"CNBC",article_publish_date,link)
-
-#             articles.append(article)
-#         except Exception as e:
-#             print("Could not parse link: ",link,e)
-#     return articles
         
 def run():
     scrapers = [CNBC("https://www.cnbc.com/")]
     articles = []
     for scraper in scrapers:
-        articles.append(scraper())
+        articles.extend(scraper())
     return articles
 
-print(run())
+print(run()[0])
 
 
