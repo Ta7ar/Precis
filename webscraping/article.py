09845ee679faab4b8ad664fd8f06a7ab80c1b546
Ta7ar <taterazay99@gmail.com>
2022-01-07 21:15:41 -0500

    Print dunder for Article class

--- webscraping/article.py
+++ webscraping/article.py
@@ -3,12 +3,20 @@ from summarizer import summarize
 # Dummy class
 # TODO: Replace with db model later
 class Article:
-    def __init__(self,title,author,body,publisher,publish_date,link) -> None:
+    def __init__(self,title,body,publisher,publish_date,link) -> None:
         self.title = title
-        self.author = author
-        self.body = body
-        self.summarized_body = summarize(body)
+        self.body = summarize(body)
         self.publisher = publisher
         self.publish_date = publish_date
         self.link = link
     
+    def __repr__(self) -> str:
+        return f'''
+        {self.title} | {self.publisher}
+
+        on {self.publish_date}
+
+        {self.body}
+
+        Read more here: {self.link}
+        '''
