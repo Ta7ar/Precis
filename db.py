69b1cbb2b050ddd045a4817cd56a403efb4791e8
Ta7ar <taterazay99@gmail.com>
2022-01-10 16:25:33 -0500

    Get list of article links in the db

--- db.py
+++ db.py
@@ -9,4 +9,10 @@ CONNECTION_STRING = os.environ.get('connection_string')
 client = pymongo.MongoClient(CONNECTION_STRING)
 db = client.get_default_database()
 article_collection = db.get_collection('articles')
-print(db.list_collection_names())
+
+def get_article_links_by_publisher(publisher):
+    links = article_collection.find({'publisher':publisher}, {'_id':False, 'link': True})
+    links = set(link['link'] for link in links)
+    return links
+
+print(get_article_links_by_publisher("CNBC"))
