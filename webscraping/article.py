d34a38ff881fcf05657880d3b76d975553186726
Ta7ar <taterazay99@gmail.com>
2022-01-11 02:11:05 -0500

    Remove publish date field

--- webscraping/article.py
+++ webscraping/article.py
@@ -1,11 +1,10 @@
 from webscraping.summarizer import summarize
 
 class Article:
-    def __init__(self,title,body,publisher,publish_date,link) -> None:
+    def __init__(self,title,body,publisher,link) -> None:
         self.title = title
         self.body = summarize(body)
         self.publisher = publisher
-        self.publish_date = publish_date
         self.link = link
     
     def __repr__(self) -> str:
--- webscraping/scraper.py
+++ webscraping/scraper.py
@@ -4,9 +4,9 @@ from webscraping.article import Article
 from db import get_article_links_by_publisher, save_articles
 
 class Scraper:
-    def __init__(self,publisher,url) -> None:
-        self.publisher = publisher
-        self.soup = Scraper._generate_soup(url)
+    def __init__(self,url) -> None:
+        self.publisher = self.__class__.__name__
+        self.homepage = Scraper._generate_soup(url)
     
     @staticmethod
     def _generate_soup(url):
@@ -22,7 +22,7 @@ class Scraper:
 
 class CNBC(Scraper):
     def get_links(self):
-        thumbnail_tags = self.soup.find('div',attrs={'id':'homepage-riverPlus'}).find_all('div',attrs={'class':'RiverHeadline-headline RiverHeadline-hasThumbnail'})
+        thumbnail_tags = self.homepage.find('div',attrs={'id':'homepage-riverPlus'}).find_all('div',attrs={'class':'RiverHeadline-headline RiverHeadline-hasThumbnail'})
         links = [thumbnail.find('a').attrs['href'] for thumbnail in thumbnail_tags]
         links = [link for link in links if link != '/pro/']
         return links
@@ -32,16 +32,14 @@ class CNBC(Scraper):
 
         title = soup.title.get_text()
 
-        publish_date = soup.find('time',attrs={'itemprop':'datePublished'}).attrs['datetime']
-
         article_p_tags = soup.find('div',attrs={'class':'ArticleBody-articleBody'}).find_all('p')
         body = ''.join([tag.get_text() for tag in article_p_tags])
 
-        return Article(title,body,self.publisher,publish_date,link)
+        return Article(title,body,self.publisher,link)
 
 class CBSNews(Scraper):
     def get_links(self):
-        a_tags = self.soup.find_all('a')
+        a_tags = self.homepage.find_all('a')
         links = [tag.attrs['href'] for tag in a_tags if "https://www.cbsnews.com/news" in tag.attrs['href']]
         return links
 
@@ -59,14 +57,13 @@ class CBSNews(Scraper):
         
         body = ''.join([tag.get_text() for tag in content_body.find_all('p')])
         title = soup.find('h1',attrs={'class':'content__title'}).get_text()
-        publish_date = soup.find('header', attrs={'class':'content__header'}).find('time').attrs['datetime']
 
-        return Article(title,body,self.publisher,publish_date, link)
+        return Article(title,body,self.publisher,link)
         
         
 def run():
     # scrapers = [CNBC("CNBC","https://www.cnbc.com/"), CBSNews("CBS", "https://www.cbsnews.com")]
-    scrapers = [CNBC("CNBC","https://www.cnbc.com/")]
+    scrapers = [CNBC("https://www.cnbc.com/")]
     for scraper in scrapers:
         # Get list of previously parsed articles already present in the db
         saved_links = get_article_links_by_publisher(scraper.publisher)
