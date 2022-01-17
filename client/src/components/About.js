15c608db70a37ccda1da06327aca7712e94e559f
Ta7ar <taterazay99@gmail.com>
2022-01-17 00:45:24 -0500

    Initial About page (without routing)

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -3,9 +3,27 @@ import React from "react";
 const About = () => {
   return (
     <div>
-      <section>
-        <h3>About</h3>
-        <p>blah blah blah</p>
+      <section style={{ borderBottom: "1px dashed grey" }}>
+        <h4>About</h4>
+        <p>
+          Precis scrapes frontpage news articles from news sites and summarizes
+          them into 5 sentences using Natural Language Processing.
+        </p>
+      </section>
+      <br />
+      <section style={{ borderBottom: "1px dashed grey" }}>
+        <h4>Disclaimer</h4>
+        <p>
+          Rights to all scraped articles belong to their respective publishers.
+        </p>
+      </section>
+      <br />
+      <section style={{ borderBottom: "1px dashed grey" }}>
+        <h4>Contributing</h4>
+        <p>
+          Want to see articles from more publishers? Other ideas to improve the
+          app? <a href="https://github.com/Ta7ar/Precis">Contribute!</a>
+        </p>
       </section>
     </div>
   );
