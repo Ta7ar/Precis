340ae8912a6d6d67ba3a3cf508e8a80ea6b5e36b
Ta7ar <taterazay99@gmail.com>
2022-01-21 15:11:38 -0500

    Show loading spinner while no articles data present

--- client/src/components/ArticleCollection.js
+++ client/src/components/ArticleCollection.js
@@ -1,26 +1,30 @@
 import React, { useState, useEffect } from "react";
 import Article from "./Article";
+import { Spinner } from "reactstrap";
 
 const ArticleCollection = () => {
-  const [isLoading, setIsLoading] = useState(true);
   const [articlesData, setArticlesData] = useState(null);
 
   useEffect(() => {
-    fetch("/api?limit=5&offset=0")
+    fetch("/api")
       .then((res) => res.json())
       .then((data) => setArticlesData(data));
   }, []);
 
-  useEffect(() => {
-    if (articlesData) {
-      setIsLoading(false);
-    } else {
-      setIsLoading(true);
-    }
-  }, [articlesData]);
-
   return (
     <div>
+      {!articlesData && (
+        <div
+          style={{
+            padding: "1rem",
+            display: "flex",
+            alignItems: "center",
+            gap: "1rem",
+          }}
+        >
+          Loading Articles <Spinner color="primary" type="grow" />
+        </div>
+      )}
       {articlesData &&
         articlesData.articles.map((article, i) => <Article>{article}</Article>)}
     </div>
