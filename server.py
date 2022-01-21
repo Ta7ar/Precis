54dc3cbedf19fa691d3a121172772e46cf7a88a5
Ta7ar <taterazay99@gmail.com>
2022-01-21 14:30:56 -0500

    Scrape articles on initial /api call

--- server.py
+++ server.py
@@ -6,8 +6,10 @@ app = Flask(__name__, template_folder="client/build", static_folder="client/buil
 
 @app.route("/api",methods=['GET'])
 def get_articles_paginated():
-    limit, offset = request.args.get('limit', 10), request.args.get('offset', 0)
+    limit, offset = request.args.get('limit', 5), request.args.get('offset', 0)
     limit, offset = int(limit), int(offset)
+    if offset == 0:
+        scraper.scrape_articles()
     data = article.get(limit,offset)
     if data is None:
         abort(Response('Offset value exceeds total number of articles available.', 404))     
@@ -16,7 +18,6 @@ def get_articles_paginated():
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    scraper.scrape_articles()
     return render_template('index.html')
 
 if __name__ == "__main__":
